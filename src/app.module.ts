98e7475c443de27a2632043a49d631d804928fb8
Lautaro Decima <decimalautaro@gmail.com>
2024-10-05 01:08:28 -0300

    feat: creating health-check controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { HealthCheckController } from './health-check/health-check.controller';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [PaymentsModule, HealthCheckModule],
+  controllers: [HealthCheckController],
 })
 export class AppModule {}
